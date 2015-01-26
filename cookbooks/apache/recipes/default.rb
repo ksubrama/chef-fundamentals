bb426160e390a64d5d54a76f29f708baf2f56caf
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-01-26 16:50:50 -0500

    Apache with some html.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -2,7 +2,20 @@
 # Cookbook Name:: apache
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, Chef
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "httpd" do
+	action :install
+end
+
+service "httpd" do
+	action [:enable, :start]
+end
+
+cookbook_file "/var/www/html/index.html" do
+	source "index.html"
+	mode "0644"
+end
