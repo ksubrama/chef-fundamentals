802b3483e4a90d5a63e5e0a5e37a2fe32ccc7b60
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-01-26 19:35:34 -0500

    Add dependencies.

--- cookbooks/motd/metadata.rb
+++ cookbooks/motd/metadata.rb
@@ -1,7 +1,8 @@
 name             'motd'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Kartik Cating-Subramanian'
+maintainer_email 'ksubramanian@chef.io'
 license          'All rights reserved'
 description      'Installs/Configures motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'pci'
