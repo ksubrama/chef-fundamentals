fb2d02bf90ba14b33f60d161afcbfc357a32cf5b
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-01-27 12:05:33 -0500

    chef generate cookbook users

---
+++ cookbooks/users/metadata.rb
@@ -0,0 +1,8 @@
+name             'users'
+maintainer       'Kartik Null Cating-Subramanian'
+maintainer_email 'ksubramanian@chef.io'
+license          'apache2'
+description      'Installs/Configures users'
+long_description 'Installs/Configures users'
+version          '0.1.0'
+
